e79a4cfb9ab6507ab3457a6ea72ba4b59d8451ad
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-01-05 15:13:16 +0000

    data_jpa

--- src/main/java/com/example/voting_system/VotingSystemApplication.java
+++ src/main/java/com/example/voting_system/VotingSystemApplication.java
@@ -1,13 +1,30 @@
 package com.example.voting_system;
 
+import com.example.voting_system.model.Role;
+import com.example.voting_system.model.User;
+import com.example.voting_system.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
 @SpringBootApplication
-public class VotingSystemApplication {
+@AllArgsConstructor
+public class VotingSystemApplication implements ApplicationRunner {
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(VotingSystemApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Set.of(Role.ROLE_USER)));
+        userRepository.save(new User("admin@gmail.com", "Admin_First", "Admin_Last", "password", Set.of(Role.ROLE_ADMIN)));
+        System.out.println(userRepository.findAll());
+
+    }
 }
