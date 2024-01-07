03e5724c6f3ecc8848141278b8c7f95aba34e0f8
Fuat_Safiulin <fuatsafiulin@gmail.com>
2024-01-07 22:12:10 +0000

    xss_validation

---
+++ src/main/java/com/example/voting_system/util/NoHtml.java
@@ -0,0 +1,22 @@
+package com.example.voting_system.util;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy =  NoHtmlValidator.class)
+@Target({METHOD, FIELD})
+@Retention(RUNTIME)
+public @interface NoHtml {
+    String message() default "{error.noHtml}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
